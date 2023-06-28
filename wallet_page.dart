6a5c2a1a6203e6fbff729af0fb445e8acfc43722
ProjectApAbtin <135759984+ProjectApAbtin@users.noreply.github.com>
2023-06-28 15:35:59 +0330

    Add files via upload

---
+++ wallet_page.dart
@@ -0,0 +1,33 @@
+import 'package:booktickets/screens/bodyone.dart';
+import 'package:flutter/material.dart';
+/*
+class profile extends StatelessWidget {
+  static String routeName ='profile';
+
+  @override
+  Widget build(BuildContext context) {
+
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+      ),
+      body: Bodyone(),
+
+
+
+    );
+  }
+}*/
+/*appBar: AppBar(
+        leading: BackButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text('My Page'),
+      ),
+      body: Center(
+        child: Text('Hello World!'),
+      ),
+*/
+
