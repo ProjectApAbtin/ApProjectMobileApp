be8aade325cd881672a7b83df53f37f5bb0a301c
ProjectApAbtin <135759984+ProjectApAbtin@users.noreply.github.com>
2023-06-28 18:54:14 +0330

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,22 +1,42 @@
+import 'package:booktickets/screens/bottom_bar.dart';
+import 'package:booktickets/screens/edit_profile_page.dart';
+import 'package:booktickets/screens/profileAvatarPage.dart';
+import 'package:booktickets/screens/wallet_page.dart';
+import 'package:booktickets/utilis/app_styles.dart';
+import 'package:booktickets/utilis/user_preferencess.dart';
 import 'package:flutter/material.dart';
 
-import 'form_screen.dart';
-
-void main(){
-  runApp(MyApp());
+ Future main() async{
+WidgetsFlutterBinding.ensureInitialized();
+await UserPreferences.init();
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
-      debugShowCheckedModeBanner:false,
+
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+
+
       theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ) ,
-      home : FormScreen(),
+        primaryColor: primary,
+        dividerColor : Colors.orangeAccent,
+      ),
+      home: const BottomBar(),
+
+
+        routes: {
+          '/UpdateInfo': (context) =>EditProfilePage(user: UserPreferences.myUser),
+        }
+
+
+
     );
   }
-
 }
